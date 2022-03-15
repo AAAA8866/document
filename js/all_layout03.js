cf86b9f4490d43df7730d8a351ff432ae9e8d771
AAAA8866 <100890250+AAAA8866@users.noreply.github.com>
2022-03-15 17:07:54 +0900

    add slick

--- js/all_layout03.js
+++ js/all_layout03.js
@@ -1,4 +1,33 @@
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icon = document.querySelector('.navbar__icon');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
+    
+});
+
+
+
 $(function() {
     ///////////
+
+
+    $('.mainSlide').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:3000,
+        Speed:300,
+        dots:false
+    });
+
+    $('.mainVisual .slideArrows i:first-child').on('click',function() {
+        $('.mainSlide').slick('slickPrev')
+    });
+    $('.mainVisual .slideArrows i:last-child').on('click',function() {
+        $('.mainSlide').slick('slickNext')
+    });
+
     ///////////
 })
